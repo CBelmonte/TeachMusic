dd43aaeb818533aee8ffe9f858582a094c80b113
Carmine Belmonte <CarmineBelmonteProg@gmail.com>
2014-03-05 10:30:49 -0800

    add

---
+++ Teach Music/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonD = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.radioButtonG = new System.Windows.Forms.RadioButton();
+            this.radioButtonF = new System.Windows.Forms.RadioButton();
+            this.radioButtonE = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ChkG = new System.Windows.Forms.CheckBox();
+            this.ChkF = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(292, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Next note";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(6, 36);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonA.TabIndex = 3;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "A";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            this.radioButtonA.CheckedChanged += new System.EventHandler(this.radioButtonA_CheckedChanged);
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(6, 59);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonB.TabIndex = 4;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "B";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonD
+            // 
+            this.radioButtonD.AutoSize = true;
+            this.radioButtonD.Location = new System.Drawing.Point(6, 106);
+            this.radioButtonD.Name = "radioButtonD";
+            this.radioButtonD.Size = new System.Drawing.Size(33, 17);
+            this.radioButtonD.TabIndex = 6;
+            this.radioButtonD.TabStop = true;
+            this.radioButtonD.Text = "D";
+            this.radioButtonD.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(6, 82);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonC.TabIndex = 5;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "C";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonG
+            // 
+            this.radioButtonG.AutoSize = true;
+            this.radioButtonG.Location = new System.Drawing.Point(6, 178);
+            this.radioButtonG.Name = "radioButtonG";
+            this.radioButtonG.Size = new System.Drawing.Size(33, 17);
+            this.radioButtonG.TabIndex = 9;
+            this.radioButtonG.TabStop = true;
+            this.radioButtonG.Text = "G";
+            this.radioButtonG.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonF
+            // 
+            this.radioButtonF.AutoSize = true;
+            this.radioButtonF.Location = new System.Drawing.Point(6, 155);
+            this.radioButtonF.Name = "radioButtonF";
+            this.radioButtonF.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonF.TabIndex = 8;
+            this.radioButtonF.TabStop = true;
+            this.radioButtonF.Text = "F";
+            this.radioButtonF.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonE
+            // 
+            this.radioButtonE.AutoSize = true;
+            this.radioButtonE.Location = new System.Drawing.Point(6, 131);
+            this.radioButtonE.Name = "radioButtonE";
+            this.radioButtonE.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonE.TabIndex = 7;
+            this.radioButtonE.TabStop = true;
+            this.radioButtonE.Text = "E";
+            this.radioButtonE.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(202, 212);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(418, 186);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(380, 405);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Score";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonG);
+            this.groupBox1.Controls.Add(this.radioButtonF);
+            this.groupBox1.Controls.Add(this.radioButtonE);
+            this.groupBox1.Controls.Add(this.radioButtonA);
+            this.groupBox1.Controls.Add(this.radioButtonD);
+            this.groupBox1.Controls.Add(this.radioButtonC);
+            this.groupBox1.Controls.Add(this.radioButtonB);
+            this.groupBox1.Location = new System.Drawing.Point(119, 250);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(77, 206);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Image = global::WindowsFormsApplication1.Properties.Resources.Staff_mnemonics;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.MinimumSize = new System.Drawing.Size(585, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(585, 205);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 453);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Score 100 points to WIN";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // ChkG
+            // 
+            this.ChkG.AutoSize = true;
+            this.ChkG.Location = new System.Drawing.Point(16, 286);
+            this.ChkG.Name = "ChkG";
+            this.ChkG.Size = new System.Drawing.Size(55, 17);
+            this.ChkG.TabIndex = 15;
+            this.ChkG.Text = "G Clef";
+            this.ChkG.UseVisualStyleBackColor = true;
+            this.ChkG.CheckedChanged += new System.EventHandler(this.ChkG_CheckedChanged);
+            // 
+            // ChkF
+            // 
+            this.ChkF.AutoSize = true;
+            this.ChkF.Location = new System.Drawing.Point(16, 310);
+            this.ChkF.Name = "ChkF";
+            this.ChkF.Size = new System.Drawing.Size(53, 17);
+            this.ChkF.TabIndex = 16;
+            this.ChkF.Text = "F Clef";
+            this.ChkF.UseVisualStyleBackColor = true;
+            this.ChkF.CheckedChanged += new System.EventHandler(this.ChkF_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 540);
+            this.Controls.Add(this.ChkF);
+            this.Controls.Add(this.ChkG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonD;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.RadioButton radioButtonG;
+        private System.Windows.Forms.RadioButton radioButtonF;
+        private System.Windows.Forms.RadioButton radioButtonE;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox ChkG;
+        private System.Windows.Forms.CheckBox ChkF;
+    }
+}
+
---
+++ Teach Music/Form1.cs
@@ -0,0 +1,313 @@
+﻿/*
+ * Author : Carmine Belmonte
+ * Version: 2.0.0
+ * date Modified 2013-7-16
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    public partial class Main : Form
+    {
+        int score = 0;
+        string ImageNum = "";
+        int randomNumber = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+                        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+        	next();
+        }
+        public void next(){
+        ImageNum = "";
+            Random random = new Random();
+            randomNumber = random.Next(0, 9);
+            button2.Visible = true;
+
+            if (ChkF.Checked==true)
+            {
+                FClefPic();
+            }
+            else
+            {
+                GClefPic();
+            }
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ChkG.Checked == true)
+            {
+                GClef();
+            }
+            else if (ChkF.Checked == true)
+            {
+                FClef();
+            }
+            next();
+        }
+        private void radioButtonA_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void FClefPic()
+        {
+            if (randomNumber == 0)
+            {
+                ImageNum = "BA";
+            }
+            else if (randomNumber == 1)
+            {
+                ImageNum = "BB";
+            }
+            else if (randomNumber == 2)
+            {
+                ImageNum = "BC";
+            }
+            else if (randomNumber == 3)
+            {
+                ImageNum = "BD";
+            }
+            else if (randomNumber == 4)
+            {
+                ImageNum = "BE";
+
+            }
+            else if (randomNumber == 5)
+            {
+                ImageNum = "BF";
+            }
+            else if (randomNumber == 6)
+            {
+                ImageNum = "BG";
+            }
+            else if (randomNumber == 7)
+            {
+                ImageNum = "BE2";
+            }
+            else if (randomNumber == 8)
+            {
+                ImageNum = "BF2";
+            }
+            else
+            {
+                ImageNum = "BG2";
+            }
+
+            label1.Image = (Image) Properties.Resources.ResourceManager.GetObject(ImageNum);
+        }
+        public void GClefPic()
+        {
+            if (randomNumber == 0)
+            {
+                ImageNum = "TA";
+            }
+            else if (randomNumber == 1)
+            {
+                ImageNum = "TB";
+            }
+            else if (randomNumber == 2)
+            {
+                ImageNum = "TC";
+            }
+            else if (randomNumber == 3)
+            {
+                ImageNum = "TD";
+            }
+            else if (randomNumber == 4)
+            {
+                ImageNum = "TE";
+
+            }
+            else if (randomNumber == 5)
+            {
+                ImageNum = "TF";
+            }
+            else if (randomNumber == 6)
+            {
+                ImageNum = "TG";
+            }
+            else if (randomNumber == 7)
+            {
+                ImageNum = "TE2";
+            }
+            else if (randomNumber == 8)
+            {
+                ImageNum = "TF2";
+            }
+            else
+            {
+                ImageNum = "TG2";
+            }
+
+            label1.Image = (Image) Properties.Resources.ResourceManager.GetObject(ImageNum);
+        }
+        public void GClef()
+        {
+            button2.Visible = false;
+            if (randomNumber == 0 && radioButtonA.Checked == true)
+            {
+                score = score + 10;
+                textBox1.Text = score.ToString();
+            }
+    else if (randomNumber == 1 && radioButtonB.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 2 && radioButtonC.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 3 && radioButtonD.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 4 && radioButtonE.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 5 && radioButtonF.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 6 && radioButtonG.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 7 && radioButtonE.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 8 && radioButtonF.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+    }
+    else if (randomNumber == 9 && radioButtonG.Checked == true)
+    {
+        score = score + 10;
+        textBox1.Text = score.ToString();
+
+
+    }
+    else
+    {
+        MessageBox.Show("OOoooo   You almost had it!  That was a tough one  Try again");
+    }
+    if (score == 100)
+    {
+        textBox1.Text = ("YOU ROCK DUDE" + Environment.NewLine + "Try again!!");
+        score = 0;
+    }
+        }
+        public void FClef()
+        {
+
+            {
+                button2.Visible = false;
+                if (randomNumber == 0 && radioButtonA.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 1 && radioButtonB.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 2 && radioButtonC.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 3 && radioButtonD.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 4 && radioButtonE.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 5 && radioButtonF.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 6 && radioButtonG.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 7 && radioButtonE.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 8 && radioButtonF.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+                }
+                else if (randomNumber == 9 && radioButtonG.Checked == true)
+                {
+                    score = score + 10;
+                    textBox1.Text = score.ToString();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("OOoooo   You almost had it!  That was a tough one  Try again");
+                }
+                if (score == 100)
+                {
+                    textBox1.Text = ("YOU ROCK DUDE" + Environment.NewLine + "Try again!!");
+                    score = 0;
+                }
+            }
+        }
+        private void ChkG_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkG.Checked==true)
+            {
+                ChkF.Checked = false;
+            }
+        }
+        private void ChkF_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkF.Checked==true)
+            {
+                ChkG.Checked = false;
+            }
+        }
+    }
+}
